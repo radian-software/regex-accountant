96637e8601da8b25e4f84c7e5957ec4bced15cba
Radon Rosborough <radon@intuitiveexplanations.com>
2024-08-21 19:21:15 -0700

    Oops

--- regex_accountant/server.py
+++ regex_accountant/server.py
@@ -21,9 +21,9 @@ class ExtTransaction(Transaction):
         s = self.description or self.description_short or self.description_details
         if part := (self.client or self.client_short):
             if self.amount > 0:
-                s += f" from {part}"
-            else:
                 s += f" to {part}"
+            else:
+                s += f" from {part}"
         if part := (
             self.payment_method or self.payment_method_short or self.payment_method_long
         ):
